42d996f3f945e571ff11eebcd5af97e5231ab89d
AlesDod02 <alessandro.dod@gmail.com>
2023-11-20 14:06:56 +0100

    Even or Odd completed

---
+++ js/main.js
@@ -0,0 +1,53 @@
+
+/*
+//PARI O DISPARI
+
+let evenOrOdd = prompt("Scegli pari o dispari?")
+//console.log(evenOrOdd)
+let number1 = parseInt(prompt("Scegli un numero da 1 a 5"))
+//console.log(number1)
+let number2 = generateRandomNumberZeroToMaxnumber(10)
+let somma = number1 + number2
+//console.log(generateRandomNumberZeroToTen)
+console.log (somma)
+let risultato = checkEvenOrOdd(somma, evenOrOdd)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//FUNZIONI
+function generateRandomNumberZeroToMaxnumber(maxnumber) {
+    let randomNumber = Math.floor(Math.random() * (maxnumber + 1));
+    return randomNumber}
+
+function checkEvenOrOdd(number, or) {
+    if (number % 2 == 0 && or == "pari"){
+
+    
+    console.log("hai vinto")
+}
+else if (number % 2 == 0 && or == "dispari"){
+
+    
+    console.log("hai perso")
+}
+else if (number % 2 != 0 && or == "dispari"){
+
+    
+    console.log("vinto")
+}
+else{
+    console.log("hai perso")
+}    
+} 
+*/
